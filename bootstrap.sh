be0ce88eb498e5292cb79af879759fae8c54f290
Rudraksh <rudraksh.tripathi8@gmail.com>
2024-09-24 21:15:27 +0530

    add p10k.zsh link + echo all outputs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
+# Function to run command and print its output
+run_command() {
+    echo "Running: $1"
+    eval "$1"
+    echo "Command finished with exit code $?"
+    echo "----------------------------------------"
+}
+
 echo "Starting EC2 instance setup..."
 
 echo "Updating and upgrading the system..."
-sudo apt update && sudo apt upgrade -y
+run_command "sudo apt update && sudo apt upgrade -y"
 
 echo "Installing zsh and other required packages..."
-sudo apt install -y zsh fzf python3-full nodejs npm build-essential python3-dev libpoppler-cpp-dev
+run_command "sudo apt install -y zsh fzf python3-full nodejs npm build-essential python3-dev libpoppler-cpp-dev"
 
 echo "Changing default shell to zsh..."
-sudo chsh -s $(which zsh) $(whoami)
+run_command "sudo chsh -s $(which zsh) $(whoami)"
 
 echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+run_command 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
 
 echo "Installing zsh plugins..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+run_command "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+run_command "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 echo "Installing Powerlevel10k theme..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+run_command "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+run_command "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc"
 
 echo "Downloading p10k configuration file..."
-curl -o ~/.p10k.zsh https://raw.githubusercontent.com/username/repo/main/.p10k.zsh
+run_command "curl -o ~/.p10k.zsh https://raw.githubusercontent.com/Rudraksh88/aws-auto/refs/heads/master/.p10k.zsh"
 
 echo "Updating plugins in .zshrc..."
-sed -i '/^plugins=/ c\plugins=(\n\tgit\n\tfzf\n\ttmux\n\tthemes\n\tsudo\n\tpython\n\tzsh-autosuggestions\n\tzsh-syntax-highlighting\n)' ~/.zshrc
+run_command "sed -i '/^plugins=/ c\plugins=(\n\tgit\n\tfzf\n\ttmux\n\tthemes\n\tsudo\n\tpython\n\tzsh-autosuggestions\n\tzsh-syntax-highlighting\n)' ~/.zshrc"
 
 echo "Sourcing the updated .zshrc..."
-source ~/.zshrc
+run_command "source ~/.zshrc"
 
 echo "Setup complete! Please log out and log back in for all changes to take effect."
